7fe018f232c729ab5676647df4385d7255ea3b33
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-05-05 18:05:31 +0200

    Added Contact Component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact  = () => {
+    return ( 
+    <div className="contact">  
+  <img src={require('./vurrukkulluk-logo.png')} alt="vurrukkulluk logo"/>
+    <div className="address">
+    <h1>Contact</h1>
+    <p>Verrukkulluk.nl</p>
+    <p>Poststraat 2b</p>
+    <p>Sittard</p>
+    <p>info@vurrukkulluk.nl</p>  
+
+    </div>
+    </div> );
+    
+}
+
+export default Contact;
+
+
