5266c5ca05bd9551138facf03292ca093b18a4a2
Daan Braas <daanbraas@gmail.com>
2023-05-08 10:30:04 +0200

    Added Details Component #8

---
+++ src/components/DetailContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Details from './Details'
+
+const DetailContent = (props) => {
+  return (
+    <div className='detailContent'>
+      <div className='detailTop'>
+        <Details />
+      </div>
+      <div className='detailBottom'>
+
+      </div>
+    </div>
+  )
+}
+
+export default DetailContent
