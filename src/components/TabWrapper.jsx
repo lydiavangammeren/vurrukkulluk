4a40ff1656a70b56478cc52de31b0f12505c79a0
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-05-10 19:03:06 +0200

    Wrapper Component three tabs #9 added Tabs and Tab component

--- src/components/TabWrapper.jsx
+++ src/components/TabWrapper.jsx
@@ -1,8 +1,27 @@
 // Wrapper component for tabs
-import React from 'react';
+import React from "react";
+import Tabs from "./Tabs";
 
 const TabWrapper = () => {
-    return (<h1>Wrapper</h1>  );
-}
- 
+  return (
+    <>
+      <h1>Tabs Demo</h1>
+      <Tabs>
+        <div label="Ingredienten">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+        <div label="Bereidingswijze">
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat
+        </div>
+        <div label="Opmerkingen">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur.
+        </div>
+      </Tabs>
+    </>
+  );
+};
+
 export default TabWrapper;
