3596aa112df310659462b4081818d08bf78dd4c8
Daan Braas <daanbraas@gmail.com>
2023-05-17 12:21:41 +0200

    Fetch Data from API

--- frontend/src/pages/DetailPage/DetailContent.jsx
+++ frontend/src/pages/DetailPage/DetailContent.jsx
@@ -1,47 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import Details from "./Details";
 import TabWrapper from "./TabWrapper";
 
 const DetailContent = () => {
+  
   const { id } = useParams();
-  const recipes = [
-    {
-      id: "1",
-      title: "Eggs & Veggies",
-      image: "EggsAndVeggies.jpg",
-      kitchen: "Nederlands",
-      type: "Vegetarisch",
-    },
-    {
-      id: "2",
-      title: "Vegan Burger",
-      image: "VeganBurger.jpg",
-      kitchen: "Amerikaans",
-      type: "Vegan",
-    },
-    {
-      id: "3",
-      title: "Sushi Rolls",
-      image: "SushiRolls.jpg",
-      kitchen: "Japans",
-      type: "Vis",
-    },
-    {
-      id: "4",
-      title: "Pizza Green",
-      image: "PizzaGreen.jpg",
-      kitchen: "Italiaans",
-      type: "Vlees",
-    },
-  ];
+
+  const [recipe, setRecipe] = useState({});
+
+    const fetchRecipe = () =>{
+      fetch(`http://localhost:3004/recipes/${id}`).then(response => response.json()).then((json) => setRecipe(json));
+    }
+
+    useEffect(() => {
+      console.log('onMount')
+      fetchRecipe();
+    }, [])
 
 
-  const recipe = recipes.find((x) => x.id === id);
   return (
     <div className='DetailContent'>
       <div className='detailTop'>
-        <Details  recipe={recipe}/>
+        <Details title={recipe.title}
+                  image={recipe.image}
+                  kitchen={recipe.kitchen}
+                  type={recipe.type}
+                  description={recipe.description}/>
       </div>
       <div className="detailBottom">
         <TabWrapper />
