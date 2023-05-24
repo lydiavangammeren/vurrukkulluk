7326d1b0a00697eed2a82c1b72a06ffbc422dbd0
Daan Braas <daanbraas@gmail.com>
2023-05-24 12:15:14 +0200

    Render speed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./layouts/Header";
 import Agenda from "./layouts/Agenda";
@@ -10,36 +10,11 @@ import Shoppingcarts from "./pages/Shoppingcart/ShoppingCarts";
 
 import "./assets/styles/main.css";
 
-import api from "./lib/recipeAPI";
+// import api from "./lib/recipeAPI";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
   // const [ search, setSearch ] = useState('');
   const [ images, setImages ] = useState([]);
-
-
-  useEffect(() => {
-    const getData = async () => {
-      try{
-        const response = await api.get('/recipes');
-        setRecipes(response.data);
-        // console.log('recipes set');
-      } catch(err){
-        if(err.response){
-          //Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        }else{
-          console.log(`Error: ${err.message}`)
-        }
-      }
-    }
-    getData();
-  }, [])
-
-  
-
 
   return (
     <div className="App">
@@ -51,7 +26,7 @@ function App() {
         </div>
         <div className="pageContainer">
           <Routes>
-            <Route path="/" element={<HomeContent recipes={recipes} setImages={setImages} />}/>
+            <Route path="/" element={<HomeContent setImages={setImages} />}/>
             <Route path="/details/:id" element={<DetailContent setImages={setImages} />}/>
             <Route path="/shoppingcart" element={<Shoppingcarts />} />
           </Routes>
