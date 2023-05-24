6633c0e84112746480565116bad1c999b40741d2
Daan Braas <daanbraas@gmail.com>
2023-05-24 17:47:35 +0200

    Smullen knop #96

--- frontend/src/pages/HomePage/Recipe.jsx
+++ frontend/src/pages/HomePage/Recipe.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Rating from '../../components/Rating/Rating';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { HiUsers } from 'react-icons/hi';
 import { MdEuro } from 'react-icons/md';
 import { VscFlame } from 'react-icons/vsc';
 
 const Recipe = ({recipe}) => {
-  
+  const navigate = useNavigate();
   return (
     <div className='Recipe' key={recipe.id}>
       {/* <Link to='details' params={{recipe: props.recipe}}> */}
@@ -33,7 +33,7 @@ const Recipe = ({recipe}) => {
       </div>
       <div className='recipe_bottom'>
         <div className='recipe_button'>
-          <button>Smullen</button>
+          <button onClick={() => navigate(`/details/${recipe.id}`)}>Smullen</button>
         </div>
         <div className='recipe_stats'>
           <div className='icon-align'>
