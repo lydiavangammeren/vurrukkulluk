49b4bd78b71437ffb3610c686074c71d3a883a23
Hana Alkadri <hanaalkadry@gmail.com>
2023-05-25 11:20:27 +0200

    #94

--- frontend/src/pages/Shoppingcart/ShoppingCarts.jsx
+++ frontend/src/pages/Shoppingcart/ShoppingCarts.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import ShoppingcartContent from "./ShoppingcartContent";
 import "./Shoppingcarts.css";
-import api from "../../lib/recipeAPI";
 import api from "../../lib/recipeAPI";
 
 const Shoppingcarts = () => {
@@ -51,38 +49,40 @@ const Shoppingcarts = () => {
   return (
     <div className="Shoppingcarts">
       <h1>Boodschappen</h1>
-      {ProductList.filter(
-        (product) => !checkedProductList.includes(product.id)
-      ).map((product) => (
-        <ShoppingcartContent
-          checked={false}
-          key={product.id}
-          id={product.id}
-          image={product.image}
-          title={product.title}
-          desc={product.desc}
-          quantity={product.quantity}
-          price={product.price}
-          removeProduct={removeProductWithId}
-          checkedProduct={toggleCheckMark}
-        />
-      ))}
-      {ProductList.filter((product) =>
-        checkedProductList.includes(product.id)
-      ).map((product) => (
-        <ShoppingcartContent
-          checked={true}
-          key={product.id}
-          id={product.id}
-          image={product.image}
-          title={product.title}
-          desc={product.desc}
-          quantity={product.quantity}
-          price={product.price}
-          removeProduct={removeProductWithId}
-          checkedProduct={toggleCheckMark}
-        />
-      ))}
+      <table>
+        {ProductList.filter(
+          (product) => !checkedProductList.includes(product.id)
+        ).map((product) => (
+          <ShoppingcartContent
+            checked={false}
+            key={product.id}
+            id={product.id}
+            image={product.image}
+            title={product.title}
+            desc={product.desc}
+            quantity={product.quantity}
+            price={product.price}
+            removeProduct={removeProductWithId}
+            checkedProduct={toggleCheckMark}
+          />
+        ))}
+        {ProductList.filter((product) =>
+          checkedProductList.includes(product.id)
+        ).map((product) => (
+          <ShoppingcartContent
+            checked={true}
+            key={product.id}
+            id={product.id}
+            image={product.image}
+            title={product.title}
+            desc={product.desc}
+            quantity={product.quantity}
+            price={product.price}
+            removeProduct={removeProductWithId}
+            checkedProduct={toggleCheckMark}
+          />
+        ))}
+      </table>
       <h2>Total: {totalPrice}</h2>
     </div>
   );
