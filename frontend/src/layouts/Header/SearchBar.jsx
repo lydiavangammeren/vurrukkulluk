4ae1702a69aad3274114cd6f9d4a7f3b9a2d5149
Daan Braas <daanbraas@gmail.com>
2023-05-26 09:40:24 +0200

    Search api call #101

--- frontend/src/layouts/Header/SearchBar.jsx
+++ frontend/src/layouts/Header/SearchBar.jsx
@@ -1,44 +1,47 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {FaSearch} from 'react-icons/fa';
 
 import api from "../../lib/recipeAPI";
 
 const SearchBar = ({setResults}) => {
-  // const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
   const [input, setInput] = useState("");
-
-  const fetchData = async (value) => {
-    try{
-      const response = await api.get('/recipes');
-      setResults(response.data.filter((recipe)=> {
-        return (
-          value &&
-          recipe &&
-          recipe.title && (
-            recipe.title.toLowerCase().includes(value) ||
-            recipe.kitchen.toLowerCase().includes(value) ||
-            recipe.type.toLowerCase().includes(value) ||
-            recipe.description.toLowerCase().includes(value)
-          )
-        )
-      }));
-
-    } catch(err){
-      if(err.response){
-        //Not in the 200 response range
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-      }else{
-        console.log(`Error: ${err.message}`)
-      }
-    }
-  }
 
   const handleChange = (value) => {
     setInput(value);
-    fetchData(value);
+    // fetchData(value);
+    setResults(items.filter((recipe)=> {
+      return (
+        value &&
+        recipe &&
+        recipe.title && (
+          recipe.title.toLowerCase().includes(value) ||
+          recipe.kitchen.toLowerCase().includes(value) ||
+          recipe.type.toLowerCase().includes(value) ||
+          recipe.description.toLowerCase().includes(value)
+        )
+      )
+    }));
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await api.get('/recipes');
+        setItems(response.data);
+      } catch(err){
+        if(err.response){
+          //Not in the 200 response range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        }else{
+          console.log(`Error: ${err.message}`)
+        }
+      }
+    }
+    fetchData();
+  }, [])
 
   return (
       <div className="input-wrapper">
