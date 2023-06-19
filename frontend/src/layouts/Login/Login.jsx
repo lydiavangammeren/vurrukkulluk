5cb02b59af9e9d45a2e440c3b1f95d4f7cb3117b
Daan Braas <daanbraas@gmail.com>
2023-06-19 21:18:28 +0200

    Login JWT preparation

--- frontend/src/layouts/Login/Login.jsx
+++ frontend/src/layouts/Login/Login.jsx
@@ -1,29 +1,63 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../../lib/recipeAPI";
 
 const Login = () => {
 
   const navigate = useNavigate()
-  const [credentials, setCredentials] = useState({email: '', password: ''});
+  // const [credentials, setCredentials] = useState({email: '', password: ''});
+  // const userRef = useRef();
+  // const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [ pwd, setPwd ] = useState('');
+  const [ errMsg, setErrMsg ] = useState('');
+
 
   const forgotPassword = () => {
     console.log("Send email to reset password")
   }  
 
-  const handleChange = e => {
-    const name = e.target.name
-    const value = e.target.value
+  // const handleChange = e => {
+  //   const name = e.target.name
+  //   const value = e.target.value
 
-    setCredentials(prev => ({
-      ...prev,
-      [name]: value
-    }))
-  }
+  //   setCredentials(prev => ({
+  //     ...prev,
+  //     [name]: value
+  //   }))
+  // }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Welkom ' + credentials.email)
-    console.log(JSON.stringify(credentials))
+    console.log('Welkom ' + user)
+    
+    try{
+      const response = await api.post('/auth/authenticate', 
+          JSON.stringify({user, pwd}),
+          {
+            headers: {'Content-Type': 'application/json' },
+            withCredentials: true
+          }
+          );
+          console.log(JSON.stringify(response?.data));
+
+          setUser('');
+          setPwd('');
+
+    } catch(err) {
+      console.log('Error in authenticate: ')
+      console.log(err?.response);
+      // if (!err?.response) {
+      //   setErrMsg('No Server Response');
+      // } else if (err.response?.status === 400) {
+      //     setErrMsg('Missing Username or Password');
+      // } else if (err.response?.status === 401) {
+      //     setErrMsg('Unauthorized');
+      // } else {
+      //     setErrMsg('Login Failed');
+      // }
+    }
   }
 
   return (
@@ -37,8 +71,9 @@ const Login = () => {
             name="email" 
             id="email" 
             type="email" 
-            value={credentials.email}
-            onChange={handleChange}
+            // ref={userRef}
+            value={user}
+            onChange={(e)=>{setUser(e.target.value)}}
             />
           <br/><br/>
           <label className="label" htmlFor="password">Wachtwoord </label>  
@@ -47,8 +82,8 @@ const Login = () => {
             name="password" 
             id="password" 
             type="password" 
-            value={credentials.password}
-            onChange={handleChange}
+            value={pwd}
+            onChange={(e)=>{setPwd(e.target.value)}}
             />
           <div className="login-bottom">
             <button className="login-button">Login</button> 
