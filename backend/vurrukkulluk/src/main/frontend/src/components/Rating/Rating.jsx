ed453aa2064ecbff4d572c62993abf37d51eda30
Daan Braas <daanbraas@gmail.com>
2023-08-07 23:52:25 +0200

    Rating #40

--- backend/vurrukkulluk/src/main/frontend/src/components/Rating/Rating.jsx
+++ backend/vurrukkulluk/src/main/frontend/src/components/Rating/Rating.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BsStarFill, BsStarHalf, BsStar} from 'react-icons/bs';
 import usePostData from "../../hooks/usePostData";
 
 const Rating = ({rating, recipeId}) => {
   // console.log('Rating: ', rating)
-  const [data, isLoaded, postData] = usePostData()
+  const [data, isLoaded, postData] = usePostData();
+  const [hoverRating, setHoverRating] = useState(null);
 
   const addRating = (rating) => {
     const body = {
@@ -16,13 +17,37 @@ const Rating = ({rating, recipeId}) => {
     postData('/ratings', body);
   }
 
+  const handleMouseEnter = (index) => {
+    setHoverRating(index + 1);
+  }
+
+  const handleMouseLeave = () => {
+    setHoverRating(null);
+  }
+
   const renderStar = (index) => {
-    if (rating >= index + 1) {
-      return <BsStarFill onClick={() =>addRating(index+1)} color='#eb8d1f' className='star'/>;
-    } else if (rating >= index + 0.5) {
-      return <BsStarHalf onClick={() =>addRating(index+1)} color='#eb8d1f' className='star'/>;
+    const hover = hoverRating !== null ? hoverRating : rating;
+    if (hover >= index + 1) {
+      return <BsStarFill 
+                onClick={() =>addRating(index+1)} 
+                color='#eb8d1f' 
+                className='star'
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}/>;
+    } else if (hover >= index + 0.5) {
+      return <BsStarHalf 
+                onClick={() =>addRating(index+1)} 
+                color='#eb8d1f' 
+                className='star'
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}/>;
     } else {
-      return <BsStar onClick={() =>addRating(index+1)} color='#eb8d1f' className='star'/>;
+      return <BsStar 
+                onClick={() =>addRating(index+1)} 
+                color='#eb8d1f' 
+                className='star'
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}/>;
     }
   }
 
